6ef0b657f57fbb73577a2ca7be2416481626b114
oldnick85 <tyapin_nikita@mail.ru>
2023-02-18 18:10:39 +0300

    [add] use i2pd version 2.46.0 by default

--- make_i2pd_yggdrasil_docker_image.sh
+++ make_i2pd_yggdrasil_docker_image.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker build \
     --build-arg UBUNTU_VERSION=22.04 \
-    --build-arg I2PD_VERSION=2.45.1 \
+    --build-arg I2PD_VERSION=2.46.0 \
     --build-arg I2PD_COMPILER=gcc \
     --build-arg YGGDRASIL_VERSION=v0.4.7 \
     --tag i2pd_yggdrasil:latest \
