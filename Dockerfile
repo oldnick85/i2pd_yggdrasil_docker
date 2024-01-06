9433a951cd082b8a0983fc8fee07ab1424f99769
oldnick85 <tyapin_nikita@mail.ru>
2024-01-06 20:28:33 +0300

    [add] use i2pd version 2.50.1 by default

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 # syntax=docker/dockerfile:1
 ARG UBUNTU_VERSION=23.10
 FROM ubuntu:${UBUNTU_VERSION} AS builder_i2pd
-ARG I2PD_VERSION=2.49.0
+ARG I2PD_VERSION=2.50.1
 ARG I2PD_COMPILER=gcc
 RUN DEBIAN_FRONTEND=noninteractive\
     apt-get update &&\
--- make_i2pd_yggdrasil_docker_image.sh
+++ make_i2pd_yggdrasil_docker_image.sh
@@ -2,7 +2,7 @@
 TAG_VERSION=$(git describe --tags || echo "unknown")
 docker build \
     --build-arg UBUNTU_VERSION=23.10 \
-    --build-arg I2PD_VERSION=2.49.0 \
+    --build-arg I2PD_VERSION=2.50.1 \
     --build-arg I2PD_COMPILER=gcc \
     --build-arg YGGDRASIL_VERSION=v0.5.4 \
     --tag i2pd_yggdrasil:${TAG_VERSION} \
