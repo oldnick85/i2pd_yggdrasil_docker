d258af16a363b023325376e3eb0613cd51cbd0c9
oldnick85 <tyapin_nikita@mail.ru>
2024-01-06 22:42:20 +0300

    [ref] change project file structure

--- support/make_docker_image.sh
+++ support/make_docker_image.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 TAG_VERSION=$(git describe --tags || echo "unknown")
-docker build \
+docker build --file="src/Dockerfile" \
     --build-arg UBUNTU_VERSION=23.10 \
     --build-arg I2PD_VERSION=2.50.1 \
     --build-arg I2PD_COMPILER=gcc \
