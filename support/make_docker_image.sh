0159ac966f1f181ccd7681721008316e51884746
oldnick85 <tyapin_nikita@mail.ru>
2024-06-05 11:07:37 +0300

    [add] use yggdrasil version 0.5.6 by default

--- support/make_docker_image.sh
+++ support/make_docker_image.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 TAG_VERSION=$(git describe --tags || echo "unknown")
 docker build --file="src/Dockerfile" \
-    --build-arg UBUNTU_VERSION=23.10 \
+    --build-arg UBUNTU_VERSION=24.04 \
     --build-arg I2PD_VERSION=2.52.0 \
     --build-arg I2PD_COMPILER=gcc \
-    --build-arg YGGDRASIL_VERSION=v0.5.5 \
+    --build-arg YGGDRASIL_VERSION=v0.5.6 \
     --tag i2pd_yggdrasil:${TAG_VERSION} \
     --tag i2pd_yggdrasil:latest \
     .
