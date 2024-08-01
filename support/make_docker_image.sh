0d58a40bb009718d9b2e95e98bad8cbfae390024
oldnick85 <tyapin_nikita@mail.ru>
2024-08-01 21:30:39 +0300

    [add] use i2pd version 2.53.0 by default

--- support/make_docker_image.sh
+++ support/make_docker_image.sh
@@ -2,7 +2,7 @@
 TAG_VERSION=$(git describe --tags || echo "unknown")
 docker build --file="src/Dockerfile" \
     --build-arg UBUNTU_VERSION=24.04 \
-    --build-arg I2PD_VERSION=2.52.0 \
+    --build-arg I2PD_VERSION=2.53.0 \
     --build-arg I2PD_COMPILER=gcc \
     --build-arg YGGDRASIL_VERSION=v0.5.6 \
     --tag i2pd_yggdrasil:${TAG_VERSION} \
