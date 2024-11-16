18ea550356ef8a73854cbc468c4e31fa5bc7c372
oldnick85 <tyapin_nikita@mail.ru>
2024-11-16 19:12:46 +0300

    [add] yggdrasil 0.5.9

--- support/make_docker_image.sh
+++ support/make_docker_image.sh
@@ -4,7 +4,7 @@ docker build --file="src/Dockerfile" \
     --build-arg UBUNTU_VERSION=24.04 \
     --build-arg I2PD_VERSION=2.54.0 \
     --build-arg I2PD_COMPILER=gcc \
-    --build-arg YGGDRASIL_VERSION=v0.5.8 \
+    --build-arg YGGDRASIL_VERSION=v0.5.9 \
     --tag i2pd_yggdrasil:${TAG_VERSION} \
     --tag i2pd_yggdrasil:latest \
     .
